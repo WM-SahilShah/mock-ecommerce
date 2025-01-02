0d18c78cb4695d5d0c085c43b1675df8904e023d
WM-SahilShah <sshah@watermelon.us>
2025-01-02 18:15:26 +0530

    collection download

--- app/routers/home.py
+++ app/routers/home.py
@@ -1,41 +1,58 @@
-from fastapi import APIRouter
-from fastapi.responses import HTMLResponse
+from fastapi import APIRouter, status
+from fastapi.responses import HTMLResponse, FileResponse
 from markdown_it import MarkdownIt
 
 router = APIRouter(tags=["Home"])
+DOCS_FILENAME = "E-Commerce API Collection.json"
 
 def html_page(content: str) -> str:
-    return """
+    return f"""
     <html>
         <head>
             <title>README.md</title>
             <style>
-                body { font-family: Arial, sans-serif; line-height: 1.6; margin: 0; padding: 0; background-color: #121212; color: #e0e0e0; margin: 40px; }
-                h1, h2, h3 { color: #ffffff; }
-                p { margin: 1em 0; }
-                code { background-color: #1e1e1e; color: #d4d4d4; padding: 0.2em 0.4em; border-radius: 4px; box-shadow: 0 0 5px rgba(0, 0, 0, 0.5); }
-                pre code { background-color: #1e1e1e; color: #d4d4d4; padding: 10px; border-radius: 4px; font-size: 1em; overflow-x: auto; }
-                table { width: 100%; border-collapse: collapse; margin: 20px 0; background-color: #1e1e1e; }
-                table, th, td { border: 1px solid #333; }
-                th, td { padding: 8px; text-align: left; color: #e0e0e0;}
-                th { background-color: #2d2d2d; }
-                a { color: #ffeb3b; text-decoration: none; }
-                a:hover { text-decoration: underline; }
+                body {{ font-family: Arial, sans-serif; line-height: 1.6; margin: 0; padding: 0; background-color: #121212; color: #e0e0e0; margin: 40px; }}
+                h1, h2, h3 {{ color: #ffffff; }}
+                p {{ margin: 1em 0; }}
+                code {{ background-color: #1e1e1e; color: #d4d4d4; padding: 0.2em 0.4em; border-radius: 4px; box-shadow: 0 0 5px rgba(0, 0, 0, 0.5); }}
+                pre code {{ background-color: #1e1e1e; color: #d4d4d4; padding: 10px; border-radius: 4px; font-size: 1em; overflow-x: auto; }}
+                table {{ width: 100%; border-collapse: collapse; margin: 20px 0; background-color: #1e1e1e; }}
+                table, th, td {{ border: 1px solid #333; }}
+                th, td {{ padding: 8px; text-align: left; color: #e0e0e0;}}
+                th {{ background-color: #2d2d2d; }}
+                a {{ color: #ffeb3b; text-decoration: none; }}
+                a:hover {{ text-decoration: underline; }}
+                .download-btn {{ display: inline-block; margin-top: 20px; padding: 10px 20px; background-color: #4CAF50; color: white; text-decoration: none; border-radius: 4px; text-align: center; }}
+                .download-btn:hover {{ background-color: #45a049; }}
             </style>
         </head>
         <body>
-            {{content}}
+            {content}
+            <a href=""/download"" class="download-btn" download>Download API Collection</a>
         </body>
     </html>
-    """.replace("{{content}}", content)
+    """
 
 @router.get("/", response_class=HTMLResponse, include_in_schema=False)
 async def read_homepage() -> HTMLResponse:
+    "Converts the markdown content from `README.md` and display it on the front page"
     try:
         with open("README.md", "r", encoding="utf-8") as f:
             readme_content = f.read()
         markdown = MarkdownIt().enable("table")
         html_content = markdown.render(readme_content)
-        return HTMLResponse(content=html_page(html_content), status_code=200)
+        return HTMLResponse(content=html_page(html_content),
+                            status_code=status.HTTP_200)
     except FileNotFoundError:
-        return HTMLResponse(content="README file not found", status_code=404)
+        return HTMLResponse(content="File 'README.md' not found",
+                            status_code=404)
+
+@router.get("/download", response_class=FileResponse, include_in_schema=False)
+async def download_file():
+    file_path = f"./files/{DOCS_FILENAME}"
+    try:
+        return FileResponse(path=file_path,
+                            filename=DOCS_FILENAME)
+    except FileNotFoundError:
+        return HTMLResponse(content=f"File '{DOCS_FILENAME}' not found",
+                            status_code=404)
